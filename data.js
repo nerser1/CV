e60588d05bd037809a3af301c1eda0c3c43e9141
Nerya <serinerya@gmail.com>
2025-04-01 21:18:09 +0200

    p

---
+++ data.js
@@ -0,0 +1,17 @@
+const page = {
+    header: {
+        h1: 'Nerya Seri',
+        h2: 'Full Stack Developer',
+        background_image: './images/backHead.jpeg'
+    },
+    navbar: {
+        links: {
+            home: './index.html',
+            skills: './skills.html',
+            education: './education.html'
+        }
+    },
+    content: {
+
+    }
+}
